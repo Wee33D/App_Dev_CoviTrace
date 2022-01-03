c6603207b933283b07afa37d2540b2cab8515548
aimanarriady <87179900+aimanarriady@users.noreply.github.com>
2022-01-03 23:47:21 +0800

    final-dashboard
    user can login to their respective dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,9 +62,7 @@
                           </li>
 
                           
-                          <li class="nav-item">
-                              <a class="nav-link text-dark" href="{{ url('register1') }}">{{ __('| Manage Healthcare Admin |') }}</a>
-                          </li>
+                    
 
                           <li class="nav-item">
                             <a class="nav-link text-dark" href="{{ route('logout') }}"
